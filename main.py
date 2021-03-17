f5a4331d34cf44f714b7af10084345d364977bde
ling1 <xiao.ling@intel.com>
2021-03-17 17:05:20 +0800

    update

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from scipy import integrate
+import math
+
+def egg_func(x):
+  return x
+
+def circle_func(x):
+  return math.sqrt(1-x**2)
+
 def main():
-  pass
+  print(integrate.quad(circle_func))
 
 if __name__ == "__main__":
   main()
