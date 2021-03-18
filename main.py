a5e1f2857a6a5356c088ed3e73232648165c91eb
ling1 <xiao.ling@intel.com>
2021-03-18 17:28:28 +0800

    updata

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ import math
 def egg_func(x):
   return x
 
-def circle_func(x):
-  return math.sqrt(1-x**2)
+def circle_func(x,r):
+  return r * math.sqrt(1/(r**2-x**2))
 
 def main():
   print(integrate.quad(circle_func))
+  ##make new empty image.
+
+
+  ##for iteration to calcaulate mapping point in the original image. Round the float value.
+
+
+  ##return the new image.
+
+
 
 if __name__ == "__main__":
   main()
